473f4c9bd9c013bc227815bea74db072950f497a
Mason Benzinger <mason.benzinger@gmail.com>
2022-06-18 14:15:12 -0500

    completed work

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,42 @@
+async function admin() {
 
+const bookListResponce = await fetch('http://localhost:3001/listBooks');
+const bookList = await bookListResponce.json();
+
+const ul = document.createElement('ul');
+
+bookList.forEach(book => {
+    const li = document.createElement('li')
+    li.textContent = book.title;
+
+    const textInput = document.createElement('input');
+    textInput.type = 'text';
+    textInput.value = book.quantity;
+    li.append(textInput)
+
+    const button = document.createElement('input');
+    button.type = 'button';
+    button.value = 'Save;'
+    button.addEventListener('click', async ()=> {
+        await fetch('http://localhost:3001/updateBook', {
+            method: 'PATCH',
+            headers: {
+                'content-Type': 'application/json'
+            },
+            body: json.stringify({
+                id: book.id,
+                quantity: textInput.value
+            }),
+        });
+    });
+    li.append(button);
+    ul.append(li);
+
+});
+
+const root = document.querySelector('#root');
+root.append(ul);
+
+}
 // Your Code Here
+admin();
